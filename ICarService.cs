7e8b7b23ce862bfa4b639536a078c5f4971bf5c1
Ertugrul <78531259+ertgrsylr@users.noreply.github.com>
2021-02-16 00:10:08 +0200

    Abstract

---
+++ ICarService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Business.Abstract
+{
+
+    public class ICarService 
+    {
+         List<Car> GetAll;
+    }
+}
